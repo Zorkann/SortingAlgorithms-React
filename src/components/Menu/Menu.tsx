1d48affea248f1f6409f5611115669cc16924607
Marcin Skowyra <mararandil95@gmail.com>
2021-12-21 02:46:25 +0100

    Grid

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -33,10 +33,12 @@ const Menu = ({
 
   return (
     <div className={styles.menu}>
-      <Button onClick={onGenerateArrayClick}> Generate Array </Button>
+      <div className={styles.button}>
+        <Button onClick={onGenerateArrayClick}> Generate Array </Button>
+      </div>
 
-      <div className={styles.slidersContainer}>
-        <div className={styles.slider}>
+      <div className={styles.sliders}>
+        <div className={styles.slider1}>
           <div className={styles.sliderLabels}>
             <label htmlFor="delay">Slow</label>
             <label htmlFor="delay">Fast</label>
@@ -51,7 +53,7 @@ const Menu = ({
           />
         </div>
 
-        <div className={styles.slider}>
+        <div className={styles.slider2}>
           <label htmlFor="length">Array Length: {arrayLength}</label>
           <input
             type="range"
@@ -65,12 +67,16 @@ const Menu = ({
       </div>
 
       <div className={styles.sort}>
-        <MenuItem isSelected={sortWith === "quick"}>
-          <Button onClick={onQuickSortClick}>Quick Sort</Button>
-        </MenuItem>
-        <MenuItem isSelected={sortWith === "bubble"}>
-          <Button onClick={onBubbleSortClick}>Bubble Sort</Button>
-        </MenuItem>
+        <div className={styles.menuItem}>
+          <MenuItem isSelected={sortWith === "quick"}>
+            <Button onClick={onQuickSortClick}>Quick Sort</Button>
+          </MenuItem>
+        </div>
+        <div className={styles.menuItem}>
+          <MenuItem isSelected={sortWith === "bubble"}>
+            <Button onClick={onBubbleSortClick}>Bubble Sort</Button>
+          </MenuItem>
+        </div>
       </div>
     </div>
   );
